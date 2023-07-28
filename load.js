e937450d60fde90dee7b41571b75742ff533a378
abiloka <31643803+abiloka@users.noreply.github.com>
2023-07-28 00:16:32 -0700

    Update load.js

--- load.js
+++ load.js
@@ -1,6 +1,6 @@
 const yearStart = 2020;
 const yearEnd = 2023;
-const totalNoOfCountriesToLoad = 400;
+const totalNoOfCountriesToLoad = 50;
 
 const margin = {top: 20, right: 120, bottom: 50, left: 50},
     svgWidth = 900,

7b9f23a9f88b9e98033ab76d76edd888468cb585
abiloka <31643803+abiloka@users.noreply.github.com>
2023-07-29 14:22:46 -0700

    Update load.js

--- load.js
+++ load.js
@@ -33,7 +33,7 @@ const innerChart = chart.append("g")
                 .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 // x,y values
-var xScale = d3.scaleLinear().range([0,width]);
+var xScale = d3.scaleTime().range([0,width]);
 var yScale = d3.scaleLinear().range([height, 0]);    
 
 // x,y axis
